bb97df1c51f0b2a7d3e9fc01693c78c2b885fe05
abolaradev <abolfazlebrahimi923395@gmail.com>
2026-09-16 02:19:44 +0330

    feat: add event and listener for token generation and delivery

---
+++ src/Listeners/SendTokenToRecipient.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Abolaradev\Otp\Listeners;
+
+use Abolaradev\Otp\Events\TokenGenerated;
+use Abolaradev\Otp\Notifications\OtpNotification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Notification;
+
+class SendTokenToRecipient
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(TokenGenerated $event): void
+    {
+        $otpDetails = $event->otpDetails;
+    
+        Notification::route('sms',$otpDetails)
+                    ->notify(new OtpNotification);
+    }
+}

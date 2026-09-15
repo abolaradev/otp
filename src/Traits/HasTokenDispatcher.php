2c1abd9c9e23e5f24b115200542941e6d2e3b1be
abolaradev <abolfazlebrahimi923395@gmail.com>
2026-09-15 18:52:40 +0330

    feat: add token dispatcher trait and TokenGenerated event dispatching

---
+++ src/Traits/HasTokenDispatcher.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Abolaradev\Otp\Traits;
+
+use Abolaradev\Otp\Events\TokenGenerated;
+
+trait HasTokenDispatcher
+{
+     /**
+     * The recipient of the OTP token.
+     */
+    private string $recipient; 
+
+    /**
+     * Set the recipient of the OTP token.
+     *
+     * @param string $recipient
+     * @return $this
+     */
+    public function to(string $recipient) :self
+    {
+        $this->recipient = $recipient;
+
+        return $this;
+    }
+
+     /**
+     * Dispatch the generated OTP token.
+     *
+     * Builds the token details for the configured recipient
+     * and dispatches the TokenGenerated event.
+     *
+     * @return void
+     */
+    public function dispatch() :void
+    {
+        $tokenDetails = $this->buildTokenDetailsFor($this->recipient);
+        event( new TokenGenerated($tokenDetails));
+    }
+}

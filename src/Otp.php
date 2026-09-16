6be347a33ae2f6289245c6343966a3325efdc58a
abolaradev <abolfazlebrahimi923395@gmail.com>
2026-09-16 23:33:30 +0330

    refactor: restructure OtpManager for issuance and verification contexts

--- src/Otp.php
+++ src/Otp.php
@@ -3,9 +3,11 @@
 namespace Abolaradev\Otp;
 
 use Abolaradev\Otp\Exceptions\OtpBadMethodCallException;
+use Abolaradev\Otp\Exceptions\OtpInvalidRecipientException;
 use Abolaradev\Otp\Services\OtpIssuance;
 use Abolaradev\Otp\Services\OtpManager;
 use Abolaradev\Otp\Services\OtpVerfication;
+use Illuminate\Support\Str;
 
 /**
  * Provides the main entry point for interacting with the OTP system.
@@ -16,13 +18,21 @@ use Abolaradev\Otp\Services\OtpVerfication;
 class Otp extends OtpManager
 {
     /**
+     * Create a new instance with the given recipient.
+     *
+     * @param string $recipient The recipient associated with the OTP.
+     */
+    public function __construct(private string $recipient = '')
+    {}
+
+    /**
      * Create a new OTP issuance context.
      *
      * @return OtpIssuance
      */
     protected function issuance(): OtpIssuance
     {
-        return (new OtpIssuance)->to($this->getRecipient());
+        return (new OtpIssuance)->to($this->recipient);
     }
 
     /**
@@ -32,29 +42,33 @@ class Otp extends OtpManager
      */
     protected function verification(): OtpVerfication
     {
-        return (new OtpVerfication)->from($this->getRecipient());
+        return (new OtpVerfication)->from($this->recipient);
     }
 
-    /**
-     * Handle calls to undefined methods.
-     *
-     * Routes the `to` and `from` methods to their corresponding
-     * OTP contexts and throws an exception for unsupported methods.
-     *
-     * @param string $name The name of the called method.
-     * @param array $arguments The arguments passed to the method.
-     *
-     * @return OtpIssuance|OtpVerfication
-     *
-     * @throws OtpBadMethodCallException
-     */
+   /**
+    * Handle dynamic calls to the OTP issuance and verification methods.
+    *
+    * Validates the recipient and routes the call to the appropriate OTP context.
+    *
+    * @param string $name The called method name.
+    * @param array $arguments The arguments passed to the method.
+    *
+    * @return OtpIssuance|OtpVerfication
+    *
+    * @throws OtpBadMethodCallException If the called method is not supported.
+    * @throws OtpInvalidRecipientException If the recipient is invalid.
+    */
     public function __call($name, $arguments)
     {
         if (!in_array($name, ['to', 'from'])) {
             throw new OtpBadMethodCallException(method: $name);
         }
 
-        $this->setRecipient($arguments[0]);
+        if (!Str::isMatch('/^09[0-9]{9}$/', $arguments[0])) {
+            throw new OtpInvalidRecipientException;
+        }
+
+        $this->recipient = $arguments[0];
 
         return match ($name) {
             'to' => $this->issuance(),

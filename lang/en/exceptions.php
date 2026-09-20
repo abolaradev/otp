4c4945f44996002aa7e55d7421e79685b83ef1c3
abolaradev <abolfazlebrahimi923395@gmail.com>
2026-09-20 16:34:32 +0330

    feat: add language files for OTP exception messages

---
+++ lang/en/exceptions.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+     'invalid_recipient' => 'The given recipient is invalid.',
+
+     'active_token' => 'An active OTP token already exists for this recipient.',
+
+     'invalid_token' => 'The given token is invalid.',
+
+     'token_rate_limit' => 'Too many OTP requests. Please try again later.',
+
+     'token_expired' => 'The OTP token has expired.',
+
+     'invalid_channel' => 'The given channel :channel is not a valid OTP channel.'
+
+];

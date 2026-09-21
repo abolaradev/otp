f7c036c1182746051890c8e19610ce4a54634a3c
abolaradev <abolfazlebrahimi923395@gmail.com>
2026-09-22 01:53:47 +0330

    feat: add package asset loader helper and register it in composer.json

---
+++ src/helpers/helpers.php
@@ -0,0 +1,10 @@
+<?php 
+
+function package_asset(string $path)
+{
+    $filePath = public_path("vendor/otp/$path");
+
+    return file_exists($filePath)
+           ? asset("vendor/otp/$path")
+           : url()->query("assets/$path" ,['ver'=>time()]);
+}
